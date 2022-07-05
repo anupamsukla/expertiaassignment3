cad1746a1990899cf00855179b559f8d74e09b60
anupamsukla <32832216+anupamsukla@users.noreply.github.com>
2022-07-05 17:53:23 +0530

    ok

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const fs = require('fs');
+const port = process.env.PORT || 5800;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const routes = require('./routes/routes.js')(app, fs);
 
-const server = app.listen(3001, () => {
+const server = app.listen(port, () => {
     console.log('listening on port %s...', server.address().port);
 });
